c49d840e32f3c7516473adcd2e645677bfce3acb
andrey_mazo <andreymazo@mail.ru>
2023-05-11 19:06:47 +0300

    документацию добавил

--- config/urls.py
+++ config/urls.py
@@ -15,15 +15,32 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-
 from django.contrib import admin
-
 from django.urls import path, include
 from django.urls import re_path as url
-
 from price.apps import PriceConfig
 from price.views import CustomAuthToken
 from rest_framework.authtoken import views
+from rest_framework import permissions
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+...
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   # permission_classes=(permissions.AllowAny,),
+)
+
 app_name = PriceConfig.name
 
 urlpatterns = [
@@ -35,11 +52,11 @@ urlpatterns = [
     # path('api-token-auth/', CustomAuthToken.as_view()),
 
     path('api/v1/rest-auth/', include('rest_auth.urls')),
-    path('api-token-auth/', views.obtain_auth_token)
+    path('api-token-auth/', views.obtain_auth_token),
     ##When using TokenAuthentication, you may want to provide a mechanism for clients to obtain a token given the username and password. REST framework provides a built-in view to provide this behaviour. To use it, add the obtain_auth_token view to your URLconf
-
-
-
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
 urlpatterns += [
